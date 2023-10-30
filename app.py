72cd4886072b34f3828a0c140b7a806faa8ed740
abhay-wyr <147714444+abhay-wyr@users.noreply.github.com>
2023-10-30 14:22:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
-from ultralytics import YOLO
+import torch
 from flask import request, Flask, jsonify
 from waitress import serve
 from PIL import Image
 
-app = Flask(__name__)
-model = YOLO("best.pt")  # Replace 0.5 with your desired confidence threshold
+app = Flask(__name)
 
-# @app.route("/")
-# def root():
-#     """
-#     Site main page handler function.
-#     :return: Content of index.html file
-#     """
-#     with open("index.html") as file:
-#         return file.read()
-
+# Load the YOLOv8 model using PyTorch
+model = torch.hub.load('ultralytics/yolov5:master', 'yolov5s', pretrained=False, path_or_model='/path/to/best.pt')
+model.eval()
+print("Loaded YOLOv8 model")
 
 @app.route("/detect", methods=["POST"])
 def detect():
-    """
-        Handler of /detect POST endpoint
-        Receives uploaded file with a name "image_file", passes it
-        through YOLOv8 object detection network and returns and array
-        of bounding boxes.
-        :return: a JSON array of objects bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
-    """
+    # Your detection code here
     if "image_file" not in request.files:
         return "No 'image_file' in request", 400
 
@@ -44,28 +32,39 @@ def detect_objects_on_image(buf):
     Function receives an image,
     passes it through YOLOv8 neural network
     and returns an array of detected objects
-    and their bounding boxes
+    sorted by the x-coordinate of the center point and y-coordinate.
     :param buf: Input image file stream
-    :return: Array of bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    :return: Array of bounding boxes in format [[x1, y1, x2, y2, object_type, probability],..]
     """
     # model = YOLO("best.pt")
-    results = model.predict(Image.open(buf), conf=0.60 )
+    results = model.predict(Image.open(buf), conf=0.60)
     print(results)
     result = results[0]
     output = []
+
+    # Create a list of objects with their x and y coordinates
+    objects_with_x_and_y = []
     for box in result.boxes:
-        x1, y1, x2, y2 = [
-            round(x) for x in box.xyxy[0].tolist()
-        ]
+        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
         class_id = box.cls[0].item()
         prob = round(box.conf[0].item(), 2)
-        output.append([
-            x1, y1, x2, y2, result.names[class_id], prob
+        x_center = (x1 + x2) / 2  # Calculate the x-coordinate of the center
+        y_center = (y1 + y2) / 2  # Calculate the y-coordinate of the center
+        objects_with_x_and_y.append([
+            x_center, y_center, x1, y1, x2, y2, result.names[class_id], prob
         ])
-    return output
+
+    # Sort objects by both x-coordinate and y-coordinate
+    sorted_objects = sorted(objects_with_x_and_y, key=lambda obj: obj[0])
+    sorted_objects = sorted(sorted_objects, key=lambda obj: obj[1])
+
+    # Extract classes from the sorted objects
+    classes_only = [obj[6] for obj in sorted_objects]
+
+    return classes_only
 
 
 
-# serve(app, host='0.0.0.0', port=8080)
+
 if __name__ == '__main__':
     app.run(debug=True)
