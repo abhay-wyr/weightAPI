8ae85da536f1b3e46c3ff72b8ededa05e460dc2c
abhay-wyr <147714444+abhay-wyr@users.noreply.github.com>
2023-10-31 12:59:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from ultralytics import YOLO
+# from ultralytics import YOLO
 from flask import request, Flask, jsonify
 # from waitress import serve
 import numpy as np
@@ -6,8 +6,15 @@ import os
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 
+from flask import Flask, request, jsonify
+import torch
+# import json
+from PIL import Image
+import numpy as np
+
 app = Flask(__name__)
-model = YOLO("bestIN.pt")  # Replace 0.5 with your desired confidence threshold
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')  # local model
+
 print("loaded  model")
 
 
@@ -35,23 +42,12 @@ def detect():
     # Return the detected bounding boxes as JSON
     return jsonify(boxes)
 
-
-def detect_objects_on_image(buf, output_folder='E:/wyr.ai/api_1/output', output_image_name='temp.jpg', confidence_threshold=0.45):
-    """
-    Function receives an image,
-    passes it through YOLOv8 neural network,
-    returns an array of detected objects,
-    and draws bounding boxes on the image to visualize the results.
-    :param buf: Input image file stream
-    :param output_folder: Path to the output folder
-    :param output_image_name: Name of the output image file
-    :param confidence_threshold: Confidence threshold for object detection
-    """
+def detect_objects_on_image(buf, output_folder='E:/wyr.ai/api_1/output', output_image_name='temp.jpg', confidence_threshold=0.65):
     ori_image = os.path.join(output_folder, 'ori.jpg')
 
     # Load the image
     image = Image.open(buf)
-    
+
     # Extract EXIF data
     exif_data = image._getexif()
     if exif_data is not None:
@@ -67,20 +63,22 @@ def detect_objects_on_image(buf, output_folder='E:/wyr.ai/api_1/output', output_
 
     image = image.rotate(0, expand=True)  # This line might not be necessary
 
-    results = model.predict(image, conf=0.80)
-    result = results[0]
+    # Perform object detection with YOLOv5
+    results = model(image)
     output = []
 
-    for box in result.boxes:
-        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
-        class_id = box.cls[0].item()
-        prob = round(box.conf[0].item(), 2)
-        class_name = result.names[class_id]
-        center_x =  int((x1 + x2) / 2)
-        center_y =  int((y1 + y2) / 2)
-        output.append([
-            center_x, center_y, class_name, prob
-        ])
+    for box in results.pred[0]:
+        x1, y1, x2, y2 = [round(x) for x in box[:4].tolist()]
+        class_id = int(box[5].item())
+        prob = round(box[4].item(), 2)
+        # Apply confidence threshold
+        if prob >= confidence_threshold:
+            class_name = results.names[class_id]
+            center_x = int((x1 + x2) / 2)
+            center_y = int((y1 + y2) / 2)
+            output.append([
+                center_x, center_y, class_name, prob
+            ])
         # Draw bounding box (if you need it)
         # ...
         # Draw a point at the center
@@ -91,10 +89,7 @@ def detect_objects_on_image(buf, output_folder='E:/wyr.ai/api_1/output', output_
     # Save the image with bounding boxes to the specified folder
     output.sort(key=lambda x: x[0])  # Sort by the first element (x-axis center)
 
-   
     return output
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
