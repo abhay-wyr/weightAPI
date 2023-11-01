73cf5ea80d8fbdbe60d791539e662f9bf938b53d
abhay-wyr <147714444+abhay-wyr@users.noreply.github.com>
2023-11-01 11:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-# from ultralytics import YOLO
 from flask import request, Flask, jsonify
-# from waitress import serve
 import numpy as np
 import os
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
-
 from flask import Flask, request, jsonify
 import torch
-# import json
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy as np
 
 app = Flask(__name__)
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')  # local model
 
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='weightBest.pt')  # local model
 print("loaded  model")
 
 
@@ -38,12 +34,10 @@ def detect():
     # cv2.imwrite('E:/wyr.ai/api_1/output/before.jpg', image_file.stream)
 
     boxes = detect_objects_on_image(image_file.stream)
-    
-    # Return the detected bounding boxes as JSON
+   
     return jsonify(boxes)
 
-def detect_objects_on_image(buf, output_folder='E:/wyr.ai/api_1/output', output_image_name='temp.jpg', confidence_threshold=0.65):
-    ori_image = os.path.join(output_folder, 'ori.jpg')
+def detect_objects_on_image(buf, confidence_threshold=0.45):
 
     # Load the image
     image = Image.open(buf)
@@ -61,36 +55,55 @@ def detect_objects_on_image(buf, output_folder='E:/wyr.ai/api_1/output', output_
             elif orientation == 8:
                 image = image.rotate(90, expand=True)
 
-    image = image.rotate(0, expand=True)  # This line might not be necessary
-
-    # Perform object detection with YOLOv5
-    results = model(image)
+    # image = image.rotate(0, expand=True)  # This line might not be necessary
+    # results = model(image)
+    image_np = np.array(image)
+    results = model(image_np)
+    tensor = results.xyxy[0]
     output = []
 
-    for box in results.pred[0]:
-        x1, y1, x2, y2 = [round(x) for x in box[:4].tolist()]
-        class_id = int(box[5].item())
-        prob = round(box[4].item(), 2)
-        # Apply confidence threshold
-        if prob >= confidence_threshold:
-            class_name = results.names[class_id]
-            center_x = int((x1 + x2) / 2)
-            center_y = int((y1 + y2) / 2)
-            output.append([
-                center_x, center_y, class_name, prob
-            ])
-        # Draw bounding box (if you need it)
-        # ...
-        # Draw a point at the center
-        # point_color = (0, 0, 255)  # Red
-        # point_radius = 5
-        # cv2.circle(image, (center_x, center_y), point_radius, point_color, -1)  # -1 means fill the circle
-
+    for box in tensor:
+        x1 , y1, x2, y2, confidence, classID = box[:6]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        confidence = round(confidence.item(), 2)
+        # print(box)
+        center_x = int((x1+x2)/2)
+        center_y = int((y1+y2)/2)
+        output.append([int(classID.item()), center_x, center_y])
+        # output.append([center_x, center_y, classID.item(), confidence])
     # Save the image with bounding boxes to the specified folder
-    output.sort(key=lambda x: x[0])  # Sort by the first element (x-axis center)
+    output.sort(key=lambda x: x[1])  # Sort by the first element (x-axis center)
+    # cv2.imwrite("output/output_image2.jpg", image_np)
+    classes_after_sort = [classID for classID, _, _ in output]
+    print(classes_after_sort)
+    return classes_after_sort
 
-    return output
+    image = Image.open(image_stream)
+        # Extract EXIF data
+    exif_data = image._getexif()
+    if exif_data is not None:
+        # Check for the orientation tag (key 274)
+        if 274 in exif_data:
+            orientation = exif_data[274]
+            if orientation == 3:
+                image = image.rotate(180, expand=True)
+            elif orientation == 6:
+                image = image.rotate(270, expand=True)
+            elif orientation == 8:
+                image = image.rotate(90, expand=True)
 
+    draw = ImageDraw.Draw(image)
+
+    for box in boxes:
+        x1, y1, x2, y2, class_name, prob, center_x, center_y = box
+
+        # Draw bounding box
+        draw.rectangle([x1, y1, x2, y2], outline="red")
+
+        # Draw center point
+        draw.point((center_x, center_y), fill="red")
+
+    return image
 if __name__ == '__main__':
     app.run(debug=True)
     # serve(app, port=5000)
